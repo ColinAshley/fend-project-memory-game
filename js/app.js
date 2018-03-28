bd5e28cd5f5489c235ef2675fe075c1ee1e63eef
Colin Ashley <colinashley306@gmail.com>
2018-03-28 14:01:22 +0100

    Initial Javascript functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 /*
  * Create a list that holds all of your cards
- */
-
+*/
+/* List of 8 pairs of card faces */
+const cardFaces = [ 'anchor', 'anchor', 'bicycle', 'bicycle', 'bolt', 'bolt', 
+                    'bomb', 'bomb', 'cube', 'cube', 'diamond', 'diamond', 
+                    'leaf', 'leaf', 'paper-plane-o', 'paper-plane-o' ];
+let openCardList = [];
 
 /*
  * Display the cards on the page
@@ -9,7 +13,22 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+// shuffle the faces
+const shuffledFaces = shuffle(cardFaces);
+// locate the deck un-numbered list and clear it 
+const deck = document.querySelector('.deck');
+deck.innerHTML='';
+// construct the list of new cards
+for (const face of shuffledFaces) {
+    deck.innerHTML +='<li class="card"><i class="fa fa-'+face+'"></i></li>';
+};
+// create a NodeList of the live cards
+const cards = document.querySelectorAll('.card');
 
+//play the game
+selectCard(); 
+
+// functions
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -25,7 +44,6 @@ function shuffle(array) {
     return array;
 }
 
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,3 +54,58 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+function selectCard() {
+	// add EventListener to each card
+  for ( card of cards ) {
+    card.addEventListener('click', function() {
+    	return( revealCard(this) );
+    });
+  }
+  // now wait on click events
+}
+
+function revealCard(currCard) {
+	// reveal selected card
+  currCard.classList.add('open', 'show');
+  addCardToOpenList(currCard);
+}
+
+function addCardToOpenList(cardToAdd) {
+	openCardList.push(cardToAdd);
+	if (openCardList.length === 2) {
+		checkForMatch();
+		openCardList=[];
+	}
+}
+
+function checkForMatch() {
+	// check if cards match
+	let cardName0 = openCardList[0].querySelector('i').classList[1];
+	let cardName1 = openCardList[1].querySelector('i').classList[1];
+	if (cardName0 == cardName1) {
+    lockCardsOpen();
+	}
+  else {
+  	releaseCards();
+  }
+}
+
+function lockCardsOpen() {
+	// remove the card's event listener
+	for (lockCard of openCardList) {
+	lockCard.removeEventListener('click', function() { 
+			return( revealCard(this) );
+		});
+		lockCard.classList.add('match');
+  	console.log('locked: ' + lockCard.querySelector('i').classList );		
+	}
+}
+
+function releaseCards() {
+	// hide card faces
+  for (releaseCard of openCardList ) {
+  	releaseCard.classList.remove('open', 'show');
+  	console.log(releaseCard.querySelector('i').classList[1]);
+  }
+}
